d2c30f6c63d8b9fa1da17f514f97bd75ab604205
LordVaderCW <hrh.james.r.stuart@gmail.com>
2026-04-27 23:16:33 +0100

    Create AssemblyInfo.vb

---
+++ Source/VisualBasic/kotor_tool/My Project/AssemblyInfo.vb
@@ -0,0 +1,13 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyVersion("1.0.3.01")> 
+<Assembly: Guid("731DB0D4-C2ED-4DB3-82C0-5C60C75A0990")>
+<Assembly: CLSCompliant(True)>
+<Assembly: AssemblyDescription("Editor for SW: KotOR")>
+<Assembly: AssemblyTrademark("Original Source Code belongs to Fred Tetra. Updates by LordVaderCW")> 
+<Assembly: AssemblyCompany("LordVaderCW")> 
+<Assembly: AssemblyTitle("KotOR Tool (2026)")> 
+<Assembly: AssemblyCopyright("(C) 2003-2005 TALII, (C) 2026 LordVaderCW")> 
+<Assembly: AssemblyProduct("KotOR Tool")>
